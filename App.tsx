0aa902976736e9672f048cd561883f1736296a92
vsakariya24-netizen <vsakariya24@gmail.com>
2026-01-17 09:07:56 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -13,7 +13,8 @@ import TermsAndConditions from './components/TermsAndConditions';
 // Public Pages
 import Home from './pages/Home';
 import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
+import ProductDetailWrapper from './pages/ProductDetailWrapper';
+
 import Contact from './pages/Contact';
 import AIFinder from './pages/AIFinder';
 import About from './pages/About';
@@ -88,7 +89,21 @@ const App: React.FC = () => {
   path="/products/:category/:subcategory" 
   element={<><Navbar /><div className="flex flex-col min-h-screen"><main className="flex-grow pt-16"><Products /></main><Footer /></div></>} 
 />
-              <Route path="/product/:slug" element={<><Navbar /><div className="flex flex-col min-h-screen"><main className="flex-grow pt-16"><ProductDetail /></main><Footer /></div></>} />
+              <Route 
+  path="/product/:slug" 
+  element={
+    <>
+      <Navbar />
+      <div className="flex flex-col min-h-screen">
+        {/* We remove pt-16 because the Wrapper pages handle their own padding */}
+        <main className="flex-grow pt-0">
+           <ProductDetailWrapper />
+        </main>
+        <Footer />
+      </div>
+    </>
+  } 
+/>
               <Route path="/manufacturing" element={<><Navbar /><div className="flex flex-col min-h-screen"><main className="flex-grow pt-16"><Manufacturing /></main><Footer /></div></>} />
               <Route path="/industrial" element={<><Navbar /><div className="flex flex-col min-h-screen"><main className="flex-grow pt-16"><Industrial /></main><Footer /></div></>} />
               <Route path="/contact" element={<><Navbar /><div className="flex flex-col min-h-screen"><main className="flex-grow pt-16"><Contact /></main><Footer /></div></>} />
@@ -97,7 +112,7 @@ const App: React.FC = () => {
               <Route path="/careers" element={<><Navbar /><div className="flex flex-col min-h-screen"><main className="flex-grow pt-16"><Careers /></main><Footer /></div></>} />
               <Route path="/blog" element={<><Navbar /><div className="flex flex-col min-h-screen"><main className="flex-grow pt-16"><Blog /></main><Footer /></div></>} />
               <Route path="/blog/:id" element={<><Navbar /><div className="flex flex-col min-h-screen"><main className="flex-grow pt-16"><BlogDetail /></main><Footer /></div></>} />
-           
+             
               <Route path="/privacy-policy" element={<PrivacyPolicy />} />
              <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
               {/* --- Admin Login --- */}
